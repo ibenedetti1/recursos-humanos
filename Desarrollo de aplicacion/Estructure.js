274f1ee62c3b9ac28eb91c674e596f6443418dd7
Sergio-Vasquez <55999397+Srg-vasquez@users.noreply.github.com>
2024-05-16 12:39:36 -0400

    Add files via upload

---
+++ Desarrollo de aplicacion/Estructure.js
@@ -0,0 +1,16 @@
+
+document.getElementById("crearCuentaBtn").addEventListener("click", function() {
+    window.location.href = "Registro.html";
+});
+
+
+
+
+document.getElementById("Sobre-Nosotros").addEventListener("click", function() {
+    window.location.href = "Sobre-Nosotros.html";
+});
+
+
+
+
+
